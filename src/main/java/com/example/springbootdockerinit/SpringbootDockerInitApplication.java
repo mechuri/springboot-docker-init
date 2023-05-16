168493fb0f471ed2925a5a8ab784dd68100894da
mechuri <churistmas@gmail.com>
2023-05-16 14:36:30 +0900

    project build

---
+++ src/main/java/com/example/springbootdockerinit/SpringbootDockerInitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerInitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootdockerinit/SpringbootDockerInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
